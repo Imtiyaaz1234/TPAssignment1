f1d82f57b92abaed29d22d01db651720f59b7153
RiyaadC <riyaadcader0@gmail.com>
2017-03-11 11:59:06 +0200

    This a test using github

---
+++ Assignment1/src/main/java/com/Assignment1/Assignment1.java
@@ -0,0 +1,14 @@
+package com.Assignment1;
+
+/**
+ * Created by student on 2017/03/11.
+ */
+public class Assignment1
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("This is the master file");
+    }
+
+}
